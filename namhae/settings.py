d47c8324598750b0da225ff2385ef657c72e9b12
sookbun <blackdew7@gmail.com>
2023-05-28 04:00:45 +0000

    secret key

--- namhae/settings.py
+++ namhae/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7sy+x1o$!_))ifq$cv)+qx4w)n3w2o+miperk2qnu&-9f%+l@5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
